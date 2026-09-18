52846d6656aad9082397d7673273a5e04f933d9d
noorulhakeema <2604715@sit.singaporetech.edu.sg>
2026-09-18 21:57:21 +0800

    main loop added & fixed errors

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,9 +1,9 @@
 inventory = 0
-failed_attempts= 0
+failed_attempts = 0
 deliveries_processed = 0
 
 def get_valid_input():
-
+    global failed_attempts
     while True:
         new_stock = input("How many stocks to add, or type 'quit' to leave: ")
 
@@ -11,7 +11,7 @@ def get_valid_input():
             return "quit"
 
         if not new_stock.isdigit():
-            print("type a positive integer.")
+            print("type a valid integer.")
             failed_attempts +=1
             continue
 
@@ -29,3 +29,21 @@ def generate_report(total_units, failed_attempts):
     print('Final Summary:')
     print('Total deliveries processed:', total_units)
     print('number of failed entries:', failed_attempts)
+
+
+while True:
+    new_stock = get_valid_input()
+
+    if new_stock =="quit":
+        generate_report(deliveries_processed, failed_attempts)
+        break
+
+    inventory = process_delivery(inventory, new_stock)
+
+    tax = calculate_tax(new_stock)
+
+    print("new stock received")
+    print("Total inventory:", inventory)
+    print("Tax for this: {:.2f}".format(tax))
+
+    deliveries_processed +=1
